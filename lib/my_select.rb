fe866dc46f71eaac0de2a9477a1a139a0c01349b
Asaf Davidov <adavidov@conncoll.edu>
2018-09-10 22:13:05 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
  # your code here!
+  index = 0
+  selected_collection = []
+  while index < collection.size
+    if yield collection[index]
+      selected_collection.push(collection[index])
+    end
+    index += 1
+  end
+  selected_collection
 end
