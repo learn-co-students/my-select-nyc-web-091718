22f5f6be1ed580352bd6ad358b3805ecfbfecab5
Casey Zimmer <czimmer@ramapo.edu>
2018-09-14 14:28:11 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,18 @@
 def my_select(collection)
- # your code here!
+ 
+ output_array = []
+ counter = 0 
+ 
+ while(counter < collection.size)
+ 
+ if(yield(collection[counter]))
+   output_array.push(collection[counter])
+ end
+ 
+  counter += 1
+ 
+ end
+ 
+ return output_array
+ 
 end
